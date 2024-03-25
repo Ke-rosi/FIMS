8fa805644acb5b7e610b582063dc7d88761a5efb
michkwaka <146165347+michkwaka@users.noreply.github.com>
2024-03-25 14:09:51 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,18 +1,9 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$fims = "fims";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $fims);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-else 
-{
+	$connect = mysqli_connect('localhost', 'root', '', 'Car');
+	if (!$connect) {
+		die(mysqli_connect_error());
+	} else {
     echo "Connection succesful";
 }
 ?>
